86b671a18d20b19ed5d085c5a158faa214576e22
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-03-21 15:06:12 +0100

    Fixed deprecation warning

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const kewords = ['close(?:s|d)?', 'fix(?:e(?:s|d))?', 'resolve(?:s|d)?'];
 
-const Octokit = require("@octokit/rest");
+const { Octokit } = require("@octokit/rest");
 
 async function run() {
 	const repository = github.context.repo;
@@ -14,36 +14,38 @@ async function run() {
 		auth: `token ${token}`,
 		userAgent: "TheBusyBiscuit/recently-closed-issues"
 	});
-	
+
 	console.log(`Retrieving the last ${per_page} commits...`);
-	
+
 	var commits = await octokit.repos.listCommits({
 		owner: repository.owner,
 		repo: repository.repo,
 		per_page: per_page
 	});
 	
+	console.log(commits);
+
 	for (let i in commits) {
 		let message = commits[i].commit.message;
-		
+
 		for (let k in keywords) {
 			let regexp = new RegExp(keywords[k] + ' #([0-9]+)');
 			let match = regexp.exec(message);
-			
+
 			while (match != null) {
 				var number = parseInt(match[1], 10);
-				
+
 				if (!(issues.includes(number))) {
 					issues.push(number);
 				}
-				
+
 				console.log(`Found closed issue #${number}`);
-				
+
 				match = regexp.exec(message);
 			}
 		}
 	}
-	
+
 	core.setOutput("issues", issues);
 }
 
